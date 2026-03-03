5e6881ea78b3749e78ec8e447d71d3670039f5d1
Leonardo <leocassio.af@gmail.com>
2026-03-03 10:40:42 -0300

    criando projeto

---
+++ ApexGrid/ApexGrid/ApexGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApexGridApp.swift
+//  ApexGrid
+//
+//  Created by Leonardo Figueiredo on 02/03/26.
+//
+
+import SwiftUI
+
+@main
+struct ApexGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
